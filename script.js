92a981d2ee1956ba27f5f5d33cf27466ad285bd6
YaroschukMaria <117159902+melnyk-anton@users.noreply.github.com>
2022-11-08 12:34:54 +0200

    asdafasdfsg
    gsdgsdgsdg

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-
+let result = document.getElementById("result");
+let answer = document.getElementById("task1");
+let button = document.getElementById("button");
+let answer2=0;
+button.addEventListener('click', (event) => {
+    if (answer.value === "github") {answer2++;result.innerText= "Ваш результат: " + answer2;button.setAttribute("disabled", true);}
+    else alert("false");
+});
+let answer3 = document.getElementById("task2");
+let button2 = document.getElementById("button2");
+button2.addEventListener('click', (event) => {
+    if (answer3.value === "рекурсія") {answer2++;result.innerText= "Ваш результат: " + answer2;button2.setAttribute("disabled", true);}
+    else alert("false");
+});
